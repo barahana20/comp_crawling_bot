7e17031be50469a7362956d8825bc0bf8f7b1c4a
barahana <barahana13@naver.com>
2022-04-15 16:06:16 +0900

    d

--- find_file.py
+++ find_file.py
@@ -12,19 +12,20 @@ def find_file_within_oneweek(file_name):
     else:
         return False
 def execute_oneweek():
-    storage_path = '/home/barahana123/knu-cse-announcements-crawler/storage'
+    storage_path = '/home/barahana123/knu-cse-announcements-crawler/markdown'
     file_paths = glob.glob(os.path.join(storage_path, '*'))
     count = 1
     for file_path in file_paths:
         if find_file_within_oneweek(os.path.basename(file_path)):
             with open(file_path, 'r') as f:
                 f_read = f.read()
-                f_read = f_read[4:f_read.index('\n\n\n\n\n\n\n\n[목록]')]
+                f_read = f_read.replace('\n\n', '\n')
+                f_read = f_read[:f_read.index('\n목록')]
                 with open('/home/barahana123/comp_crawling_bot/oneweek.txt', 'a') as g:
                     g.write(str(count)+'. '+f_read)
                     g.write('\n\n-------------------------------------------\n\n')
             count+=1
 if __name__ == '__main__':
-    with open('./oneweek.txt', 'w') as g:
+    with open('/home/barahana123/comp_crawling_bot/oneweek.txt', 'w') as g:
         pass
     execute_oneweek()
